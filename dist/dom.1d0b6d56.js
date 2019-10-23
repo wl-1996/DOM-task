055a6dceeb1c224804dd02ef79e32679ed35a698
WK <741341649@qq.com>
2019-10-23 19:57:35 +0800

    修改部分文本内容

--- dist/dom.1d0b6d56.js
+++ dist/dom.1d0b6d56.js
@@ -174,7 +174,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61378" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61875" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
